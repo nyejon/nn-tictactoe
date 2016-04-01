1d59dc9378b1bacf5d0976f1ac9ca02433ae885f
jondoe <jondoexyzzy@gmail.com>
2016-04-01 19:22:17 +0200

    Starting to play around with keras NN

--- src/nn-tictactoe.py
+++ src/nn-tictactoe.py
@@ -1,6 +1,9 @@
 import numpy
 import theano
 import theano.tensor as T
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
 rng = numpy.random
 
 
@@ -61,6 +64,22 @@ def draw_board():
 def AI(player):
     #board_probability = range(9)
     board_probability = [rng.ranf() for i in xrange(9)]
+    model = Sequential()
+    # Dense(64) is a fully-connected layer with 64 hidden units.
+    # in the first layer, you must specify the expected input data shape:
+    # here, 20-dimensional vectors.
+    model.add(Dense(64, input_dim=9, init='uniform'))
+    model.add(Activation('tanh'))
+    model.add(Dense(64, init='uniform'))
+    model.add(Activation('tanh'))
+    model.add(Dense(10, init='uniform'))
+    model.add(Activation('tanh'))
+
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd)
+
+    score = model.evaluate(X_test, y_test, batch_size=16)
     return board_probability
 
 player = 1
